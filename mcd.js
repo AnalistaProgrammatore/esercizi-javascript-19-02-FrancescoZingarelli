041868417a971f66705aaf619ad4822ccdfc11e6
FrancescoZingarelli <59926996+FrancescoZingarelli@users.noreply.github.com>
2020-02-23 16:48:17 +0100

    Update mcd.js

--- mcd.js
+++ mcd.js
@@ -1,3 +1,20 @@
-/**
-* Scrivere qui il codice delle funzioni mcd(x, y) e mcdRecursive(x, y)
-**/
+let mcd = function(a, b) {
+    if (b === 0) return a;
+    let r = a % b;
+    if (r === 0) return b;
+    while (true) {
+        a = b;
+        b = r;
+        r = a % b;
+        if (r === 0) return b;
+    }
+};
+
+
+
+let mcdRecursive = function(a, b) {
+    if (b === 0) return a;
+    let r = a % b;
+    if (r === 0) return b;
+    return mcdRecursive(b, r);
+};
