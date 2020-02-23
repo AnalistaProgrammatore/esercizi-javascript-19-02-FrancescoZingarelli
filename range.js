37d1a714c7090c9bf2a52ab77bd53c2ccf7c6567
FrancescoZingarelli <59926996+FrancescoZingarelli@users.noreply.github.com>
2020-02-23 17:39:53 +0100

    Update range.js

--- range.js
+++ range.js
@@ -1,3 +1,29 @@
-/**
-* Scrivere qui il codice dell'esercizio 1 del capitolo 4 di eloquentJS
-**/
+let range = function(start, end, step = 1) {
+    let rangeArray = [];
+    if(step > 0) {
+        for (let i = start; i <= end; i = i + step) {
+            rangeArray.push(i);
+        };
+    } else {
+        for (let i = start; i >= end; i = i + step) {
+            rangeArray.push(i);
+        };
+    };
+    return rangeArray;
+};
+
+
+
+let sum = function(inputArray) {
+    let result = 0;
+    for (element of inputArray) {
+        result = result + element;
+    };
+    return result;
+};
+
+
+
+console.log(range(1, 10));
+console.log(range(5, 2, -1));
+console.log(sum(range(1, 10)));
