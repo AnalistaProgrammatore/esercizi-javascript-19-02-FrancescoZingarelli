8ffb2a8cad62919a2a4d26658b0b8ae93863d9cf
FrancescoZingarelli <59926996+FrancescoZingarelli@users.noreply.github.com>
2020-02-23 19:16:59 +0100

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -1,4 +1,29 @@
-/**
-* Scrivere qui il codice dell'esercizio 2 del capitolo 4 di eloquentJS
-* Vi ricordo che le funzioni DEVONO ESSERE PURE e implementate una in modo IMPERATIVO e una in modo DICHIARATIVO
-**/
+// IO L'HO FATTO COSI', CHE CREDO SIA IMPERATIVA, LA VERSIONE DICHIARATIVA MO VEDIAMO SE RIESCO A FARLA
+
+let reverseArray = function(inputArray) {
+    let outputArray = [];
+    for (let i = 0 ; i < inputArray.length; i++) {
+        outputArray.push(inputArray[inputArray.length - 1 - i]);
+    };
+    return outputArray;
+};
+
+
+
+let reverseArrayInPlace = function(inputArray) {
+    let arrayLength = inputArray.length;
+    let outputArray = [];
+    for (let i = 0; i < arrayLength; i++) {
+        outputArray[i] = inputArray[arrayLength -1 -i];
+    };
+    for (let i = 0; i < arrayLength; i++) {
+        inputArray[i] = outputArray[i];
+    };
+};
+
+
+
+console.log(reverseArray(["A", "B", "C"]));
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
