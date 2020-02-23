3c98a0ead0e2bcbeaf1ecbbd6659907a226587b2
FrancescoZingarelli <59926996+FrancescoZingarelli@users.noreply.github.com>
2020-02-23 17:42:12 +0100

    Update list.js

--- list.js
+++ list.js
@@ -1,3 +1,53 @@
-/**
-* Scrivere qui il codice dell'esercizio 3 del capitolo 4 di eloquentJS
-**/
+let arrayToList = function(inputArray) {
+    let arrayToListR = function(key, value) {
+        if (key === inputArray.length - 1)  {
+            return {value, rest: null}
+        } else 
+        return {value, rest: arrayToListR(key + 1, inputArray[key +1])};
+    }
+    return arrayToListR(0, inputArray[0]);
+};
+
+
+
+let listToArray = function(list) {
+    let listToArrayR = function({value, rest}) {
+        if (rest === null) {
+            return [value];
+        } else {
+            return [value].concat(listToArrayR(rest));
+        }
+    };
+    return listToArrayR(list);
+};
+
+
+
+let prepend = function(value, rest) {
+    newList = {value, rest};
+    return newList;
+};
+
+
+
+let nth = function(list, numero) {
+    let nthR = function({value, rest}, position) {
+        if (position === numero) {
+            return value;
+        } else {
+            list = {rest};
+            return nthR(rest, position + 1);
+        }
+    };
+    return nthR(list, 0);
+};
+
+
+
+console.log(arrayToList([10, 20]));
+console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(prepend(10, prepend(20, null)));
+console.log(nth(arrayToList([10, 20, 30]), 1));
+
+console.log(JSON.stringify(arrayToList([1, 2, 3, 4])));
+// (se faccio un array più lungo di tre elementi non me li mostra in vscode se non faccio JSON.stringify)
